e0d621fb05e14db0eb816cbf8410d6e4a890d120
Mike0325 <85435378+Mike0325-c@users.noreply.github.com>
2024-04-04 14:58:13 +1100

    Update eddy.py

--- eddy.py
+++ eddy.py
@@ -1,1 +1,24 @@
 
+import sys
+import re
+
+
+
+
+
+def command_q():
+  print('this is command q')
+
+
+def command_p():
+  print('this is command q')
+
+def command_d():
+  print('this is command q')
+
+def command_s():
+  print('this is command q')
+
+
+def command_n():
+  print('this is command q')
